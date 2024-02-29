9d5c1b2875a1ca8873236af28de0e886b2d1f47a
davidlcassidy <github@davidlcassidy.com>
2024-02-29 01:46:35 +0100

    Add user profile

---
+++ backend/src/main/java/com/davidlcassidy/reportbuilder/payload/UserDTO.java
@@ -0,0 +1,32 @@
+package com.davidlcassidy.reportbuilder.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String hashedPassword;
+    private String hashedAuthenticationToken;
+    private ZonedDateTime authenticationTokenExpiration;
+
+    private String firstName;
+    private String lastName;
+    private String position;
+
+}
